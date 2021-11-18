6c5e3d06a9419c92c87c6d6b18f22cdf93fb1688
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-11-19 00:02:49 +0200

    completed fundamentals Exercise_04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,29 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        byte myByte = 12;
+        System.out.println("byte myByte is: " + myByte);
 
-        // write your code below
+        short myShort = 15;
+        System.out.println("short myShort is: " + myShort);
+
+        int myInt = 1;
+        System.out.println("int myInt is: " + myInt);
+
+        long myLong = 134l;
+        System.out.println("long myLong is: " + myLong);
+
+        float myFloat = 18.1234f;
+        System.out.println("float myFloat is: " + myFloat);
+
+        double myDouble = 12.765431;
+        System.out.println("double myDouble is: " + myDouble);
+
+        boolean myBoolean = true;
+        System.out.println("boolean myBoolean is: " + myBoolean);
+
+        char myChar = 'k';
+        System.out.println("char myChar is: " + myChar);
 
     }
 
