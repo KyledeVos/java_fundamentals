32ca6b38ff9a56464156b658801e6154f897e6d5
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-11-27 13:43:55 +0200

    completed conditonal_loops Exercise_02

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -11,14 +11,46 @@ package labs_examples.conditions_loops.labs;
  *
  */
 
+import java.util.Scanner;
+
 public class Exercise_02 {
+
+
+
+
 
     public static void main(String[] args) {
 
         // 1) create scanner (don't forget to import Scanner!)
+        Scanner scanner = new Scanner(System.in);
         // 2) prompt user
+        System.out.println("Please enter  number from 1 - 7 to return day of the week");
         // 3) assign input to variable as int
+        int dayOfWeek = scanner.nextInt();
+        scanner.nextLine();
+
         // 4) write completed code here
 
+        if(dayOfWeek > 0 && dayOfWeek < 8){
+            if(dayOfWeek == 1){
+                System.out.println("Monday");
+            } else if(dayOfWeek == 2){
+                System.out.println("Tuesday");
+            } else if(dayOfWeek == 3){
+                System.out.println("Wednesday");
+            } else if(dayOfWeek == 4){
+                System.out.println("Thursday");
+            } else if(dayOfWeek == 5){
+                System.out.println("Friday");
+            } else if(dayOfWeek == 6){
+                System.out.println("Saturday");
+            } else if(dayOfWeek == 7){
+                System.out.println("Sunday");
+            }
+        } else {
+            System.out.println("Other");
+        }
+
     }
+
 }
