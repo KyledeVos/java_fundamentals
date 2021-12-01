3483474f60f206adb160cccc7fc48bd926a9f8b6
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-01 20:55:46 +0200

    completed arrays Exercise_07

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,61 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        System.out.println("ArrayList demonstrating customers online food choices:");
+
+        System.out.println();
+        System.out.println("Demonstrating Adding Items to ArrayList:");
+        arrayList.add("Milk");
+        arrayList.add("Bread");
+        arrayList.add("Pasta");
+        arrayList.add("Sugar");
+        arrayList.add("Beef");
+        arrayList.add("Chicken");
+        arrayList.add("Soap");
+        arrayList.add("Coffee");
+        System.out.println();
+
+        System.out.println("Items Selected: ");
+        for(int i = 0; i< arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+
+        System.out.println();
+        System.out.println("Demonstrating Removing an Item");
+        arrayList.remove("Beef");
+        arrayList.remove(0);
+        System.out.println();
+
+        System.out.println("Items Selected: ");
+        for(int i = 0; i< arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+
+        System.out.println();
+        System.out.println("Demonstrating searching for an Item");
+        System.out.println("Index of Pasta: " + arrayList.indexOf("Pasta"));
+        System.out.println();
+
+        System.out.println("Demonstrating changing of Item: Bread to Milk");
+        arrayList.set(0, "Milk");
+        System.out.println("Items Selected: ");
+        for(int i = 0; i< arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+        System.out.println();
+
+        System.out.println("Checking if list is empty");
+        System.out.println(arrayList.isEmpty());
+        System.out.println();
+
+        System.out.println("Demonstating clearing of ArrayList; client emptying cart online");
+        arrayList.clear();
+
+        System.out.println("Is Cart Empty: " + arrayList.isEmpty());
+
+
+    }
 }
