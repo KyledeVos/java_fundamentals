ab22c09d1d825d5430797964a3e8722268b596ea
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-01 19:36:07 +0200

    Completed Arrays Exercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -11,5 +13,33 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] userArray = new int[10];
+        int sum = 0;
+        double average = 0;
+
+        for(int i = 0; i< userArray.length; i++){
+            System.out.println("Please enter input " + (i + 1));
+            int input = scanner.nextInt();
+            scanner.nextLine();
+            userArray[i] = input;
+            sum+=input;
+        }
+
+        average = ((double) sum)/userArray.length;
+
+        System.out.println("User Data Inputs Are:");
+        for(int i = 0; i< userArray.length; i++){
+            System.out.println(userArray[i]);
+        }
+
+        System.out.println("Sum of all User Values: " + sum);
+        System.out.println("Average of User Values: " + average);
+
+    }
+
+
 
 }
