31826bdaa68d4a3f039439071d49d671164beaf8
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-01 20:19:06 +0200

    completed arrays Exercise_04

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -10,4 +10,17 @@ package labs_examples.arrays.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        int[][] iregArray = {{1} , {2 , 3}, {4, 5, 6}, {7 ,8 , 9 , 10} , { 11, 12, 13} , {14 , 15}, {16}};
+
+        for(int[] i : iregArray){
+            for (int j : i){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
