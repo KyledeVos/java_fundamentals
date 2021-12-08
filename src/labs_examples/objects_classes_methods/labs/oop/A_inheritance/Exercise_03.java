aff5a335e733e467ebc53aaf193763afc266e9fe
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-08 13:34:54 +0200

    Completed OOP A_Inheritance Exercise_03

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -4,6 +4,11 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Why does the output print in the order it does?
  *
  * You answer:
+ * Class C_1 is a child class of class B_1 which is a child class of class A_1
+ * The constructor for all classes has been set to print that it was called
+ *
+ * In the controller class we only see the constructor for class C_1 was called, but it must first call
+ * the constructor for class B_1 which must first call the constructor for class A_1
  *
  */
 
