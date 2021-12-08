38155e5cd9af5625f3a1cf160860cb265b943863
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-08 13:31:13 +0200

    completed OOP Exercise_02

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,12 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
+ * The object is defined to be of type 'Parent Class' and set to a new instance of the child class
+ * Objects declared in this manner do not have access to a child's class fields or methods unless
+ * this method in the child class overrides a method from the parent class
  *
+ * The output is 10 (declared in the parent class) as the object can only access fields from the parent
+ * class
  */
 class A {
     int i = 10;
