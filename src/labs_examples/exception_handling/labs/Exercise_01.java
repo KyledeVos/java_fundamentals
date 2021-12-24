28f2e4cfd37681cb12103a7c04718926469fe002
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-24 22:39:11 +0200

    Completed Exceptions Exercise_01

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -6,4 +6,19 @@ package labs_examples.exception_handling.labs;
  *      1) Demonstrate a try/catch.
  *
  */
+public class Exercise_01{
+
+    public static void main(String[] args) {
+
+        try{
+            System.out.println(5/0);
+        } catch(ArithmeticException caught){
+            System.out.println("Cannot perform a division by zero");
+
+        }
+
+    }
+
+
+}
 
