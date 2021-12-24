2dc5df24d54808775912f9ee039b192df1850736
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-24 22:51:00 +0200

    Completed Exceptions Exercise_02

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -1,10 +1,39 @@
 package labs_examples.exception_handling.labs;
 
+import java.util.ArrayList;
+
 /**
  * Exception Handling Exercise 2:
  *
  *      Demonstrate a try/catch with multiple catch statements.
  *
  */
+
+public class Exercise_02{
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Benjamin");
+        names.add("Ned");
+        names.add("Mandy");
+        names.add("Freddy");
+        String name = "";
+        names.add(name);
+
+        for(int i = 0; i< names.size() + 1; i++){
+            for(int j = 0; j< names.size(); j++) {
+                try {
+                    System.out.println("Name: " + names.get(i) + " has characters: ");
+                    System.out.println(names.get(i).charAt(j) + " ");
+
+                } catch(StringIndexOutOfBoundsException caught) {
+                    System.out.println(caught.toString());
+                } catch(IndexOutOfBoundsException	 caught){
+                    System.out.println("Data does not exist");
+                }
+            }
+        }
+    }
+}
 
 
