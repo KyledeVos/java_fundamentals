4fbd1b4cfd417e3674df40c9a4c44881a2781ed8
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-25 11:41:48 +0200

    Completed Exceptions Exercise_05

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -9,9 +9,28 @@ import java.util.Arrays;
  *
  */
 
-class Example {
+class  Example {
 
     public static void main(String[] args) {
+
+        try{
+            covidRoomCapacity();
+        } catch(ArrayIndexOutOfBoundsException exc){
+            System.out.println("No more room size detail available at this time");
+        }
+
+    }
+
+    public static void covidRoomCapacity() throws ArrayIndexOutOfBoundsException{
+
+        int[] roomArea = {0, 20, 40, 60, 80, 100, 120, 150, 180, 210};
+        int count = 0;
+
+        for(int i = 2; i<400; i+=2){
+            System.out.println("Total number of people in room of size: " + roomArea[roomArea.length-(count+1)] +
+                    " square metres is: " + roomArea[roomArea.length-(count+1)]/i);
+            count++;
+        }
 
     }
 
