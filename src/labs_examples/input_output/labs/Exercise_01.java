7568494f3cd891f65f735f353d1118ca39f32d74
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2021-12-29 12:18:27 +0200

    Completed Input_Output Exercise_01 with MorningRoutine as original file

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,48 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        String source = "src/labs_examples/input_output/labs/MorningRoutine.txt";
+        String destination = "src/labs_examples/input_output/labs/Exercise_01_Copy.txt";
 
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+
+        FileOutputStream fileOutputStream = null;
+
+        try{
+            inputStream = new FileInputStream(source);
+            bufferedInputStream = new BufferedInputStream(inputStream);
+
+            fileOutputStream = new FileOutputStream(destination);
+
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while((bytesRead = bufferedInputStream.read(buffer)) != -1){
+                fileOutputStream.write(buffer);
+            }
+
+        } catch(IOException exc){
+            System.out.println(exc.toString());
+        }
+
+        try{
+            inputStream.close();
+        } catch(IOException exc){
+            System.out.println(exc.toString());
+        }
+
+        try{
+            bufferedInputStream.close();
+        } catch(IOException exc){
+            System.out.println(exc.toString());
+        }
+
+        try{
+            fileOutputStream.close();
+        } catch(IOException exc){
+            System.out.println(exc.toString());
+        }
 
     }
 }
