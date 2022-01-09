dc3465b9167502cb6316cd0595a14ab808835fc6
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-01-09 22:20:38 +0200

    Completed MultiThreading Exercise_02

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,38 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+
+        Counter counter = new Counter();
+
+    }
+}
+
+class Counter extends Thread{
+
+    int count;
+
+    public Counter(){
+        count = 0;
+        start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Counting from 0");
+
+        for(int i = 0; i< 50; i++){
+            System.out.println(count);
+            count++;
+
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException exc){
+                System.out.println(exc.getMessage());
+            }
+        }
+    }
+}
