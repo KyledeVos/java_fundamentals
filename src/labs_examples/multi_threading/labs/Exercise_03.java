1952d2797766fe08eea23c65379ca9ca3b064a72
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-01-09 22:23:25 +0200

    Completed MultiThreading Exercise_03

--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -5,3 +5,48 @@ package labs_examples.multi_threading.labs;
  *
  *      In one of the previous exercises, demonstrate changing the priority of a thread
  */
+
+//from Exercise_01
+
+public class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        Thread first = new Thread(new Count(), "first");
+        first.setPriority(10);
+        first.start();
+
+        Count count = new Count();
+        Thread second = new Thread(count, "second");
+        second.setPriority(1);
+        second.start();
+
+    }
+}
+
+class NewCount implements Runnable{
+
+    int count;
+
+    public NewCount(){
+        count = 0;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running New Count");
+        for(int i = 0; i< 50; i++){
+            System.out.println(count);
+            count++;
+
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException exc){
+                System.out.println(exc.toString());
+            }
+        }
+    }
+}
+
+
+
