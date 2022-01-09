b59281176e5fa208f5bf325b1f7eaf65ffb1e7ee
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-01-09 22:17:19 +0200

    completed MultiThreading Exercise_01

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -8,4 +8,41 @@ package labs_examples.multi_threading.labs;
  *
  */
 
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        Thread first = new Thread(new Count(), "first");
+        first.start();
+
+        Count count = new Count();
+        new Thread(count, "second").start();
+
+    }
+}
+
+class Count implements Runnable{
+
+    int count;
+
+    public Count(){
+        count = 0;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running New Count");
+        for(int i = 0; i< 50; i++){
+            System.out.println(count);
+            count++;
+
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException exc){
+                System.out.println(exc.toString());
+            }
+        }
+    }
+}
+
 
