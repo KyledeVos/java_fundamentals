29851695be51b9cd901f8d681767885608a14cd6
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-01-17 17:53:19 +0200

    Completed Genberics Exercise_02

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -8,6 +8,29 @@ package labs_examples.generics.labs;
  */
 import java.util.ArrayList;
 
+public class Exercise_02 {
+    public static void main(String[] args) {
+
+        Demo demo = new Demo();
+
+        Double[] testArray = {1.5 , 1.2, 4.5 , 100.6, 34.9};
+        Integer[] intArray = {4, 5, 6, 1 , 12, 3456};
+
+        System.out.println("Sum of Double Array: " + demo.sumArray(testArray));
+        System.out.println("\nSum of Integer Array: " + demo.sumArray(intArray));
+
+    }
+}
+
 class Demo{
 
+    public <T extends Number> double sumArray(T[] inputArray){
+
+        double sum = 0;
+
+        for(T search : inputArray){
+            sum+= search.doubleValue();
+        }
+        return  sum;
+    }
 }
