f515dfe8325ecbb580fa6151d6b684af5e2a000c
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-01-17 17:46:42 +0200

    Completed Generics Exercise_01

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,56 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+public class Exercise_01 {
+    public static void main(String[] args) {
+
+        MathCalculator<Double, Double> mathCalculator = new MathCalculator<>(3.5, 2.7);
+        System.out.println("First Number is: " + mathCalculator.getFirstNumber());
+        System.out.println("Second Number is: " + mathCalculator.getSecondNumber());
+        System.out.println("\n" + "Changing second number to 5");
+        mathCalculator.setSecondNumber(5.0);
+        System.out.println("Second Number is: " + mathCalculator.getSecondNumber());
+        System.out.println("Sum is: " + mathCalculator.sum());
+
+        MathCalculator<Integer, Integer> intObject = new MathCalculator<>(6, 8);
+        System.out.println("\nNow With Integers");
+        System.out.println("First Number is: " + intObject.getFirstNumber());
+        System.out.println("Second Number is: " + intObject.getSecondNumber());
+        System.out.println("Sum is: " + intObject.sum());
+
+    }
+}
+
+class MathCalculator<T extends Number, V extends Number>{
+
+    T firstNumber;
+    V secondNumber;
+
+    public MathCalculator(T firstNumber, V secondNumber){
+
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+
+    }
+
+    public T getFirstNumber() {
+        return firstNumber;
+    }
+
+    public V getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setFirstNumber(T firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(V secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public T sum(){
+       return firstNumber;
+    }
+}
+
