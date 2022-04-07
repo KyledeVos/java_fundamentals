c536106f0fa34f86b17c7873c768b395d0d12cc8
KyledeVos <57043473+KyledeVos@users.noreply.github.com>
2022-04-07 12:18:10 +0200

    Completed LinkedLists Exercise_01 with Car class

---
+++ src/labs_examples/datastructures/linkedlist/labs/Car.java
@@ -0,0 +1,47 @@
+package labs_examples.datastructures.linkedlist.labs;
+
+public class Car {
+
+    private String name;
+    private String engineSize;
+    private int year;
+
+    public Car(String name, String engineSize, int year) {
+        this.name = name;
+        this.engineSize = engineSize;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(String engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", engineSize='" + engineSize + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
--- src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_01.java
@@ -1,5 +1,9 @@
 package labs_examples.datastructures.linkedlist.labs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  *      LinkedLists - Exercise_01
  *
@@ -21,3 +25,81 @@ package labs_examples.datastructures.linkedlist.labs;
  *      14) listIterator()
  *      15) clear()
  */
+
+public class Exercise_01{
+
+    public static void main(String[] args){
+
+        //1) create a LinkedList (from Java's libraries)
+        LinkedList<Car> carList = new LinkedList<>();
+
+        //2) add()
+        carList.add((new Car("Ferrari" , "V8" , 2019)));
+        carList.add((new Car("Datsun" , "1200" , 2016)));
+        carList.add(new Car("Aston Martin" , "V10" , 2017));
+        carList.add((new Car("Bugatti" , "W16" , 2012)));
+        carList.add(new Car("Toyoto Corolla" , "1400" , 2015));
+        carList.add((new Car("Hilux" , "2400" , 2020)));
+        carList.add(new Car("Mustang" , "V8" , 2019));
+        carList.add(new Car("Hyundai Getz" , "1600" , 2009));
+
+        //3) addAll()
+        ArrayList<Car> secondList = new ArrayList();
+        secondList.add((new Car("Renault Sandero" , "1400" , 2015)));
+        secondList.add((new Car("Nissan Sentra" , "1800" , 2018)));
+        secondList.add(new Car("Bentley Continental" , "3000" , 2021));
+        carList.addAll(secondList);
+
+        //4) addFirst()
+        carList.addFirst(new Car("Toyota Prius" , "electric" , 2010));
+
+        //5) addLast()
+        carList.addLast(new Car("Jaguar E-Type" , "V6" , 1981));
+
+        //6) getFirst()
+        System.out.println("Car first in list: " + carList.getFirst().toString() + "\n");
+
+        //7) getLast()
+        System.out.println("Car last in list: " + carList.getLast().toString() + "\n");
+
+        //8) get()
+        System.out.println("Car third in list: " + carList.get(2).toString() + "\n");
+
+        //9) set()
+
+        System.out.println("Car third in list has been changed to: ");
+        carList.set((2), new Car("Subaru" , "2200" , 2015));
+        System.out.println(carList.get(2).toString() + "\n");
+
+        //10) push()
+        carList.push(new Car("Lamborghini ", "V10", 2020));
+        System.out.println("New car in front of list after push: " + carList.getFirst().toString() + "\n");
+
+        //11) pop()
+        System.out.println("Car popped off the front of the list: " + carList.pop().toString() + "\n");
+
+        //12) remove()
+        carList.remove(carList.size()-1);
+
+        //13) contains()
+        Car def = carList.get(4);
+        System.out.println("Check if carlist contains: " + def.getName() + " : " + carList.contains(def) + "\n");
+
+        //14) listIterator()
+
+        ListIterator listIterator = carList.listIterator();
+        System.out.println("List of Cars: " + "\n");
+        while(listIterator.hasNext()){
+            System.out.println(listIterator.next().toString());
+        }
+        System.out.println();
+
+        //15) clear()
+        carList.clear();
+        System.out.println("After clear is carList Empty: " + carList.isEmpty());
+
+    }
+
+}
+
+
